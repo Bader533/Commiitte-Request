325c3d03861be2f4c5e117b1301d0e0165a51397
hani-tabash <hani.a.tabash@gmail.com>
2021-11-18 08:56:39 +0200

    #1:uplode project

---
+++ resources/views/request_committee/create.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="d-flex flex-column flex-lg-row mb-17">
+        <!--begin::Content-->
+        <div class="flex-lg-row-fluid me-10 me-lg-20">
+            <!--begin::Form-->
+            <form action="" class="form mb-15" method="post" id="kt_careers_form">
+                <!--begin::Input group-->
+                <div class="row mb-5">
+                    <!--begin::Col-->
+                    <div class="col-md-6 fv-row">
+                        <!--begin::Label-->
+                        <label class="required fs-5 fw-bold mb-2">عدد افراد اللجنة</label>
+                        <!--end::Label-->
+                        <!--begin::Input-->
+                        <input type="number" class="form-control form-control-solid" placeholder="" name="first_name" />
+                        <!--end::Input-->
+                    </div>
+                    <!--end::Col-->
+                    <!--begin::Col-->
+                    <div class="col-md-6 fv-row">
+                        <!--end::Label-->
+                        <label class="required fs-5 fw-bold mb-2">تاريخ البدء</label>
+                        <!--end::Label-->
+                        <!--end::Input-->
+                        <input type="date" class="form-control form-control-solid" placeholder="" name="last_name" />
+                        <!--end::Input-->
+                    </div>
+                    <!--end::Col-->
+                </div>
+                <!--end::Input group-->
+                <!--begin::Input group-->
+                <div class="row mb-5">
+                    <!--begin::Col-->
+                    <div class="col-md-6 fv-row">
+                        <!--begin::Label-->
+                        <label class="required fs-5 fw-bold mb-2">مدة عمل اللجنة</label>
+                        <!--end::Label-->
+                        <!--begin::Input-->
+                        <input class="form-control form-control-solid" placeholder="" name="email" />
+                        <!--end::Input-->
+                    </div>
+
+                </div>
+
+                <!--end::Input group-->
+                <!--begin::Input group-->
+                <div class="d-flex flex-column mb-5">
+                    <label class="fs-6 fw-bold mb-2">سبب انعقاد اللجنة</label>
+                    <textarea class="form-control form-control-solid" rows="2" name="experience" placeholder=""></textarea>
+                </div>
+
+                <!--end::Input group-->
+                <!--begin::Separator-->
+                <div class="separator mb-8"></div>
+                <!--end::Separator-->
+                <!--begin::Submit-->
+                <button type="submit" class="btn btn-primary" id="kt_careers_submit_button">
+                    <!--begin::Indicator-->
+                    <span class="indicator-label">طلب تشكيل لجنة</span>
+                    <span class="indicator-progress">انتظر من فظلك
+                        <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                    <!--end::Indicator-->
+                </button>
+                <!--end::Submit-->
+            </form>
+            <!--end::Form-->
+
+        </div>
+
+    </div>
+
+@endsection
