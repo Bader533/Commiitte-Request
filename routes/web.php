28e7e88220f7ced2be590df4e1b8379c33899ef9
hani-tabash <hani.a.tabash@gmail.com>
2021-11-21 09:29:11 +0200

    app,route

--- routes/web.php
+++ routes/web.php
@@ -20,35 +20,33 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+//عرض صفحة انشاء طلب
 Route::get('/request_committee/create', [App\Http\Controllers\C_request_committee::class, 'create'])
 ->name('request_committee.create')->middleware('auth');
-
+// عرض صفحة الوكيل للموافقة على الطلب
 Route::get('/request_committee/form_request', [App\Http\Controllers\C_request_committee::class, 'formrequest'])
 ->name('request_committee.formrequest')->middleware('auth');
-
+// عرض صفحة الطلبات عند الشؤون الادارية
 Route::get('/request_committee/form_request_affairs', [App\Http\Controllers\C_request_committee::class, 'formrequestaffairs'])
 ->name('request_committee.formrequestaffairs')->middleware('auth');
-
+// عرض صفحة تشكيل اعضاء اللجان
 Route::get('/request_committee/committee_members', [App\Http\Controllers\C_request_committee::class, 'committee_members'])
 ->name('request_committee.committeemembers')->middleware('auth');
-
-//عند الضعط على رز اعداد قرار تشكيل الجنة
+//الشؤون الادارية -عند الضعط على زر اعداد قرار تشكيل لجنة
 Route::get('/request_committee/decision to prepare a committee', [App\Http\Controllers\C_request_committee::class, 'decision_committee'])
 ->name('request_committee.decision_committee')->middleware('auth');
-
+// صفحة لعرض جميع اللجان المشاركة فيها الادارة
 Route::get('/request_committee/decision to form committees at the relevant departments', [App\Http\Controllers\C_request_committee::class, 'decision_to_form_committees'])
 ->name('request_committee.decision_to_form_committees')->middleware('auth');
-
+//صفحة لعرض التقرير النهائي لبيانات اللجان
 Route::get('/request_committee/final report for the adoption of the committees data', [App\Http\Controllers\C_request_committee::class, 'final_report_for_the_adoption'])
 ->name('request_committee.final_report_for_the_adoption')->middleware('auth');
-
+//صفحة لعرض تفاصيل اللجنة
 Route::get('/request_committee/View committee details', [App\Http\Controllers\C_request_committee::class, 'view_committee_details'])
 ->name('request_committee.view_committee_details')->middleware('auth');
-
+//صفحة لعرض اعضاء اللجنة و القونين يمكن من خلالها اعتماد اللجنة و رفعها للوكيل
 Route::get('/request_committee/composition of committee members', [App\Http\Controllers\C_request_committee::class, 'composition_of_committee_members'])
 ->name('request_committee.composition_of_committee_members')->middleware('auth');
-
-
+//صفحة لعرض كافة تفاصيل وبيانات اللجنةواعضائهاوالنصوصوالقوانين
 Route::get('/request_committee/committee formation requests', [App\Http\Controllers\C_request_committee::class, 'committee_formation_requests'])
 ->name('request_committee.committee_formation_requests')->middleware('auth');
