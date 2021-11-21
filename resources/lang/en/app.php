42d4f8c6b9ede702aa28844cc1f7832aa2556e6a
hani-tabash <hani.a.tabash@gmail.com>
2021-11-21 10:23:43 +0200

    app

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -5,8 +5,10 @@ return [
     'sign_out' => 'تسجيل الخروج',
     'Submit_commission_request'=>'تقديم طلب لجنة',
     'request_a_commission'=>' طلب لجنة',
-    'notifications'=>'الاشعارات',
-    'reports'=>'اشعار',
+    'notifications'=>'الإشعارات',
+    'reports'=>'إشعار',
+    'View_All'=>'عرض الكل',
+    'Settings'=>'الإعدادات',
     'Request_to_form_an_administrative_affairs_committee'=>' طلب تشكيل لجنة الشئون الادارية',
 
 ];
