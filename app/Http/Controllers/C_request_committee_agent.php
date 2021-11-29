f0b65cb4e7bf56ba33cced73f17935f8b78415f5
Hani Tabash <59541396+hani-tabash@users.noreply.github.com>
2021-11-29 10:36:28 +0200

    ..

--- app/Http/Controllers/C_request_committee_agent.php
+++ app/Http/Controllers/C_request_committee_agent.php
@@ -8,40 +8,46 @@ use PDO;
 
 class C_request_committee_agent extends Controller
 {
-   //الموافقة على الطلب من قبل الوكيل
-   public function Post_req_agent()
-   {
-       $pdo = DB::getPdo();
-       $ID = 10;
-       $STATUS_TB_ID=2;
-       $REQUEST_COMMITTEE_TB=2;
-       $USERS_TB_ID=2;
-       $NAME='الوكيل يوافق على الطلب';
-   }
-   //عرض جميع الطلبات للوكيل
-   public function formrequest()
-   {
-       $sql = "begin
+    //الموافقة على الطلب من قبل الوكيل
+    public function Post_req_agent()
+    {
+        $pdo = DB::getPdo();
+        $STATUS_TB_ID = 2;
+        $REQUEST_COMMITTEE_TB = 2;
+        $USERS_TB_ID = 2;
+        $NAME = 'الوكيل يوافق على الطلب';
+        $stmt = $pdo->prepare("begin HANI.Post_req_agent(:STATUS_TB_ID,:REQUEST_COMMITTEE_TB,:USERS_TB_ID,:NAME); end;");
+        $stmt->bindParam(':STATUS_TB_ID', $STATUS_TB_ID, PDO::PARAM_INT);
+        $stmt->bindParam(':REQUEST_COMMITTEE_TB', $REQUEST_COMMITTEE_TB, PDO::PARAM_STMT);
+        $stmt->bindParam(':USERS_TB_ID', $USERS_TB_ID, PDO::PARAM_INT);
+        $stmt->bindParam(':NAME', $NAME, PDO::PARAM_STR, 200);
+        $stmt->execute();
+
+        
+    }
+    //عرض جميع الطلبات للوكيل
+    public function formrequest()
+    {
+        $sql = "begin
            HANI.Get_req_agent(:pageNumber,:req);
        end;";
-       return DB::transaction(function ($conn) use ($sql) {
-           $pdo = $conn->getPdo();
-           $stmt = $pdo->prepare($sql);
-           $pageNumber = 1;
+        return DB::transaction(function ($conn) use ($sql) {
+            $pdo = $conn->getPdo();
+            $stmt = $pdo->prepare($sql);
+            $pageNumber = 1;
 
-           $stmt->bindParam(':pageNumber', $pageNumber, PDO::PARAM_INT);
-           $stmt->bindParam(':req', $req, PDO::PARAM_STMT);
+            $stmt->bindParam(':pageNumber', $pageNumber, PDO::PARAM_INT);
+            $stmt->bindParam(':req', $req, PDO::PARAM_STMT);
 
-           $stmt->execute();
+            $stmt->execute();
 
-           oci_execute($req, OCI_DEFAULT);
-           oci_fetch_all($req, $array, 0, -1, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
-           oci_free_cursor($req);
+            oci_execute($req, OCI_DEFAULT);
+            oci_fetch_all($req, $array, 0, -1, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
+            oci_free_cursor($req);
 
-           return view('request_committee.formrequest', [
-               'result' => $array
-           ]);
-       });
-   }
-   
+            return view('request_committee.formrequest', [
+                'result' => $array
+            ]);
+        });
+    }
 }
